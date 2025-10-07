e889b9af4acdce738954486fe6bc7779124a5cdd
Jeff Bowen <jblz@users.noreply.github.com>
2025-10-07 12:17:25 -0400

    Hard-code Mojeek entries in expected format

--- update_list.py
+++ update_list.py
@@ -1,5 +1,6 @@
 """Update the search_engines.pickle list contained within the package.
 Use this before deploying an update"""
+
 from __future__ import absolute_import, division, print_function
 
 import os
@@ -23,7 +24,19 @@ def main():
     url = urlopen('https://raw.githubusercontent.com/matomo-org/searchengine-and-social-list/master/SearchEngines.yml')
     yaml = YAML(typ='safe', pure=True)
     matomo_engines = yaml.load(url)
-    with open(filename, 'w') as json_file:
+
+    # Hard-code Mojeek entries in expected format
+    # see: https://github.com/Parsely/serpextract/pull/23
+    matomo_engines["Mojeek"] = [
+        {
+            "backlink": "search?q={k}",
+            "hiddenkeyword": ["/^$/", "/", "/\\/search(\\?.*)?/", "/\\/url\\?.*/"],
+            "params": ["q"],
+            "urls": ["mojeek.com", "www.mojeek.com"],
+        }
+    ]
+
+    with open(filename, "w") as json_file:
         json.dump(matomo_engines, json_file, indent=2, sort_keys=True)
 
     print('Saved {} search engine parser definitions to {}.'
